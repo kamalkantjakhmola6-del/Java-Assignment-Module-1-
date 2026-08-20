a6abc51e9f97b943b90a1e3798347fbe868f9f27
kamalkantjakhmola6-del <kamalkantjakhmola6@gmail.com>
2026-08-20 19:37:07 +0530

    1. Identifier Naming Practice

---
+++ kamal.java
@@ -0,0 +1,20 @@
+public class StudentDetails {
+
+    public static void main(String[] args) {
+
+        // Declaring variables
+        String studentName = "Kamal";
+        int rollNumber = 101;
+
+        int javaMarks = 85;
+        int pythonMarks = 90;
+        int dbmsMarks = 88;
+
+        // Printing student details
+        System.out.println("Student Name: " + studentName);
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("Java Marks: " + javaMarks);
+        System.out.println("Python Marks: " + pythonMarks);
+        System.out.println("DBMS Marks: " + dbmsMarks);
+    }
+}
